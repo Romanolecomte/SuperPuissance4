b8ec7989eac6949c757f216e4a29a71435cf454e
Romain Lecomte <romainlecomte@epfedufr>
2022-11-18 14:39:08 +0100

    Creation du projet Puissance 4

---
+++ src/puissance4_morel_lecomte/Puissance4_MOREL_LECOMTE.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package puissance4_morel_lecomte;
+
+/**
+ *
+ * @author romai
+ */
+public class Puissance4_MOREL_LECOMTE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
