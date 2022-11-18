cd8405e04f83f7ca498ce147d8a536e99b40e823
Romain Lecomte <romainlecomte@epfedufr>
2022-11-18 15:05:17 +0100

    Creation du projet Puissance 4
    + 1ere classe : Jeton

---
+++ src/puissance4_morel_lecomte/Jeton.java
@@ -0,0 +1,43 @@
+package puissance4_morel_lecomte;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author romai
+ */
+public class Jeton {
+    
+    private String couleur;
+
+    public Jeton(String couleur) {
+        this.couleur = couleur;
+    }
+    
+    public String getCouleur() {
+        return couleur;
+    }
+    
+    @Override
+    public String toString() {
+        
+        if (couleur == "rouge"){
+            System.out.println("R");
+        }
+        else{
+            System.out.println("J");
+        }
+        return "Jeton{" + "couleur=" + couleur + '}';
+    }
+    
+    
+    
+    
+    
+    
+    
+}
+
