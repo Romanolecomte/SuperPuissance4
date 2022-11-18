81fb2ac8dc5d66f6c4959c02262ffc5bf0bad178
lea <lea.morel@epfedu.fr>
2022-11-18 17:10:28 +0100

    creation classe joueur

---
+++ src/puissance4_morel_lecomte/joueur.java
@@ -0,0 +1,40 @@
+package puissance4_morel_lecomte;
+
+import java.util.ArrayList;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author leamo
+ */
+public class joueur {
+    private String nom;
+    public String couleur;
+    public ArrayList<Jeton> reserveJetons;
+    public int nombreJetonsRestants;
+    public int nombreDesintegrateurs;    
+    
+    public joueur (String Nom){
+        nom=Nom;
+    }
+    
+    public void affecterCouleur(String lacouleur){
+        couleur=lacouleur;
+    }
+    
+    public void ajouterJeton(Jeton jeton){
+        reserveJetons.add(jeton);
+    } 
+    
+    public void obtenirDesintegrateur(){
+        
+    }
+}
+
+
+     
+
