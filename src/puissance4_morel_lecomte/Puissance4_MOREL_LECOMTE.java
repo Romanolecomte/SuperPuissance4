81e11ff391704a7a9dbedebf83587cc8a76fc693
lea <lea.morel@epfedu.fr>
2022-12-07 10:23:32 +0100

    fin lancer partie et main

--- src/puissance4_morel_lecomte/Puissance4_MOREL_LECOMTE.java
+++ src/puissance4_morel_lecomte/Puissance4_MOREL_LECOMTE.java
@@ -14,7 +14,16 @@ public class Puissance4_MOREL_LECOMTE {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        joueur joueur1 = new joueur ("Romain");
+        joueur joueur2 = new joueur ("Léa");
+        
+        Partie partie1 = new Partie (joueur1,joueur2);
+        
+        partie1.initialiserPartie();
+        
+        partie1.lancerPartie();
+        
        
     }
     
